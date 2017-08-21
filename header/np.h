931e3c8db2906dd907bc3135a43cde70bfa86837
Tsung-Hsiang Wu <aplmnbvcxz@gmail.com>
2017-08-21 14:24:51 +0100

    Add Socket() and Bind() definition

--- header/np.h
+++ header/np.h
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 /*
  * DNS lookup / service name lookup :
@@ -21,7 +22,27 @@ int GetAddrInfo(const char *node, const char *service, const struct addrinfo *hi
 
 /*
  * Free one or more addrinfo structures returned by getaddrinfo(),
+ * res: a linked list that contains a list of addrinfo structure
  * */
 void FreeAddrInfo(struct addrinfo *res);
+
+/*
+ * creates an endpoint for communication/IO and returns a file descriptor that refers to that endpoint.
+ * domain: a communication domain
+ * type: the indicated type, which specifies the communication semantics
+ * protocol: specifies a particular protocol to be used with the socket.  
+ *           Normally only a single protocol exists to support a particular 
+ *           socket type within a given protocol family 
+ * */
+int Socket(int domain, int type, int protocol);
+
+/*
+ * Bind a name to a socket. 
+ * bind() assigns the address specified by 'addr' to the socket referred to by the file descriptor 'sockfd'.
+ * sockfd: socket file descriptor returned from socket()
+ * my_addr: the address information that is to be bound to socket descriptor (IP / port number)
+ * addrlen: size of my_addr
+ * */
+int Bind(int sockfd, struct sockaddr *my_addr, int addrlen);
 
 #endif
--- helper/np.cpp
+++ helper/np.cpp
@@ -24,8 +24,47 @@
 
 /*
  * Free one or more addrinfo structures returned by getaddrinfo(),
+ * res: a linked list that contains a list of addrinfo structure
  * */
 void FreeAddrInfo(struct addrinfo *res)
 {
 	freeaddrinfo(res);
 }
+
+/*
+ * creates an endpoint for communication/IO and returns a file descriptor that refers to that endpoint.
+ * type: the indicated type, which specifies the communication semantics
+ * protocol: specifies a particular protocol to be used with the socket.
+ *	     Normally only a single protocol exists to support a particular
+ *           socket type within a given protocol family
+ *
+ * */
+int Socket(int domain, int type, int protocol)
+{
+	int fd; // file descriptor
+	if((fd = socket(domain, type, protocol)) == -1)
+	{
+		fprintf(stderr, "socket error");
+		exit(EXIT_FAILURE);
+	}
+	return fd;
+}
+
+/*
+ * Bind a name to a socket.
+ * bind() assigns the address specified by 'addr' to the socket referred to by the file descriptor 'sockfd'.
+ * sockfd: socket file descriptor returned from socket()
+ * my_addr: the address information that is to be bound to socket descriptor (IP / port number)
+ * addrlen: size of my_addr
+ * */
+int Bind(int sockfd, struct sockaddr *my_addr, int addrlen)
+{
+	int status;
+	if((status = bind(sockfd, my_addr, addrlen) == -1))
+	{
+                fprintf(stderr, "binding error");
+                exit(EXIT_FAILURE);
+	}
+	return status;
+}
+
